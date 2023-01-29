94c7dcf0ef3bcab3eb5604f4da510b23b3c200ca
José Lin <lin-jose@outlook.com>
2023-01-28 22:51:12 -0300

    Set logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { SiHomeassistant } from "react-icons/si";
 export default function Header() {
   return (
-    <header className="col-span-3 max-h-24 bg-gray-900 text-white">
-      Header
+    <header className="col-span-3 flex max-h-24 items-center justify-between bg-[#333333] px-24 text-white">
+      <div className="flex flex-shrink-0 flex-row items-center text-3xl">
+        <span className="flex flex-shrink-0 items-center text-orange-400">
+          <SiHomeassistant className="mr-1" /> Zr
+        </span>
+        <span className="text-white">Home</span>
+      </div>
+      <div>Dropdown Menu</div>
     </header>
   );
 }
