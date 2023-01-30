fd551ca0ce4ebd983ee5496e94fea422668c485e
José Lin <lin-jose@outlook.com>
2023-01-30 16:50:09 -0300

    Refactor DepartmentMenu

--- src/components/TopDisplayButtons.jsx
+++ src/components/TopDisplayButtons.jsx
@@ -3,11 +3,11 @@ import { useContext } from "react";
 import TopDisplayButton from "./TopDisplayButton";
 
 export default function TopDisplayButtons() {
-  const { productMenuOpen } = useContext(ProductContext);
+  const { departmentMenuOpen } = useContext(ProductContext);
   return (
     <div
-      className={`absolute right-6 top-6 flex translate-x-96 gap-5 transition-opacity duration-500 ${
-        !productMenuOpen
+      className={`absolute right-6 top-6 flex gap-5 transition-opacity duration-500 ${
+        !departmentMenuOpen
           ? "translate-x-96 opacity-0"
           : "translate-x-0 opacity-100"
       }`}
